e29ebb055ca3cc6d4cd3c643a3296bd8c72bf41c
paurangpure8 <38027722+paurangpure8@users.noreply.github.com>
2018-05-07 16:54:41 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,8 @@
 docker stop chefContainer1
+sleep 5
 docker rm chefContainer1
+sleep 5
 docker rmi $(docker images |grep 'paurangpure/chefimage')
+sleep 5
+docker rmi $(docker images |grep 'registry.hub.docker.com/paurangpure/chefimage')
 sudo chef-client
