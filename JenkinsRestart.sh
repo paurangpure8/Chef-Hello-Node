e907d1def15426add695678a739942645402ff26
paurangpure8 <38027722+paurangpure8@users.noreply.github.com>
2018-05-17 15:24:50 +0530

    Update JenkinsRestart.sh

--- JenkinsRestart.sh
+++ JenkinsRestart.sh
@@ -8,7 +8,7 @@ JENKINS_NODE_NR=`hostname -s | cut -d'0' -f2`
 JENKINS_NODENAME="${JENKINS_NODE_LINUX}_slave_node_${JENKINS_NODE_NR}"
 CHECKED=`curl --silent $JENKINS_URL/computer/$JENKINS_NODENAME/api/json | grep -Po '"offline":true' | cut -d':' -f2`
 if [[ "$CHECKED" == "true" ]]; then
-    //    service jenkins-slave restart
+      #  service jenkins-slave restart
         echo "INFO: $JENKINS_NODENAME was offline and are now restarted"
         exit 0
 fi
